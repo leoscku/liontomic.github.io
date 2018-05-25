dabd4997e362c68adf1000188a08843a963d0797
Leo (Shih-Chuan) Ku <leo.sc.ku@gmail.com>
2018-05-25 01:20:07 -0700

    added three project cards

--- js/index.js
+++ js/index.js
@@ -1,3 +1,9 @@
+$(function() {
+  // load html files in correct divs
+  $("#navbar-container").load("navbar.html");
+});
+
 $(document).ready(function(){
     $('.sidenav').sidenav();
+     $('.materialboxed').materialbox();
 });
